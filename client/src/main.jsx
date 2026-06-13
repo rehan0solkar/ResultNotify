2b46ccfb5736acebacfec8b99f1174a9ea5b424f
rehan0solkar <rhnslkr@gmail.com>
2026-06-13 20:02:26 +0530

    Revert "Add Vercel Analytics"
    This reverts commit e1dc395ea68ddbc72394671e6e390c153bcc55b8.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Toaster } from "react-hot-toast";
-import { Analytics } from '@vercel/analytics/react';
 import './index.css'
 
 import App from './App.jsx'
@@ -20,7 +19,6 @@ createRoot(document.getElementById('root')).render(
     },
   }}
 /><App />
-<Analytics />
     </GoogleOAuthProvider>
   </StrictMode>
 )
