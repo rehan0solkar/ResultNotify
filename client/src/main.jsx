e1dc395ea68ddbc72394671e6e390c153bcc55b8
rehan0solkar <rhnslkr@gmail.com>
2026-06-13 19:44:35 +0530

    Add Vercel Analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from '@vercel/analytics/react';
 import './index.css'
 
 import App from './App.jsx'
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')).render(
     },
   }}
 /><App />
+<Analytics />
     </GoogleOAuthProvider>
   </StrictMode>
 )
